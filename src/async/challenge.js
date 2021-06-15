b0069f3bfc8712b4cffa1d8d9d0876ccbc7f796e
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-14 23:30:19 -0500

    :art: Update

---
+++ src/async/challenge.js
@@ -0,0 +1,20 @@
+const fetchData = require('../utils/fetchData');
+const API = 'https://rickandmortyapi.com/api/character/';
+
+const anotherFunc = async (url) => {
+  try {
+    const data = await fetchData(url);
+    const character = await fetchData(`${API}${data.results[0].id}`);
+    const origin = await fetchData(character.origin.url);
+
+    console.log(data.info.count);
+    console.log(character.name);
+    console.log(origin.dimension);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+console.log('Before');
+anotherFunc();
+console.log('After.');
