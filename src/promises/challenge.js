fc48f857e6bc96080b717b905b12cfd63f3a2d52
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-14 22:18:16 -0500

    :building_construction: Add Utils

--- src/promises/challenge.js
+++ src/promises/challenge.js
@@ -1,21 +1,5 @@
-const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-
 const API = 'https://rickandmortyapi.com/api/character/';
-
-const fetchData = (url_api) => {
-  return new Promise((resolve, reject) => {
-    const xhttp = new XMLHttpRequest();
-    xhttp.open('GET', url_api, true);
-    xhttp.onreadystatechange = () => {
-      if (xhttp.readyState === 4) {
-        xhttp.status === 200
-          ? resolve(JSON.parse(xhttp.responseText))
-          : reject(new Error('Error ', url_api));
-      }
-    };
-    xhttp.send();
-  });
-};
+const fetchData = require('../utils/fetchData');
 
 fetchData(API)
   .then((data) => {
