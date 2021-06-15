62779eb3c3eca0ca30c6608431dfa92fd9f4e334
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-14 21:06:15 -0500

    :art: Update

--- others/callbacks/challange.js
+++ others/callbacks/challange.js
@@ -1,11 +1,17 @@
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
-const fetchData = (url, cb) => {
-  let xhttps = new XMLHttpRequest();
-  xhttps.open('GET', url, true);
-  xhttps.onreadystatechange = (event) => {
-    if (xhttps.readyState === 4) {
-      // pass
+const fetchData = (apiURL, callback) => {
+  const xhttp = new XMLHttpRequest();
+  xhttp.open('GET', apiURL, true);
+  xhttp.onreadystatechange = (event) => {
+    if (xhttp.readyState === 4) {
+      if (xhttp.status === 200) {
+        callback(null, JSON.parse(xhttp.responseText));
+      }
+    } else {
+      const error = new Error(`Error ${apiURL}, Status: ${xhttp.status}`);
+      return callback(error, null);
     }
   };
+  xhttp.send();
 };
