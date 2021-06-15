7a58111dad20f62a8164c2d815ab680755c1a77e
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-14 21:42:52 -0500

    :art: Promise.all()

--- src/promises/promiseAll.js
+++ src/promises/promiseAll.js
@@ -1,3 +1,9 @@
+const somethingWillHappen = (condition) => {
+  return new Promise((resolve, reject) =>
+    condition ? resolve('Nice Vim bro.') : reject('F.')
+  );
+};
+
 const somethingWillHappenTwo = (condition) => {
   return new Promise((resolve, reject) => {
     condition
@@ -6,6 +12,6 @@ const somethingWillHappenTwo = (condition) => {
   });
 };
 
-somethingWillHappenTwo(true)
+Promise.all([somethingWillHappen(true), somethingWillHappenTwo(true)])
   .then((response) => console.log(response))
   .catch((error) => console.error(error));
